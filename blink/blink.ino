bb7c5e0a03262ddd3ccd067e05c493a9c403c092
Teresa Jeon <teresajeon@seoulchristian.org>
2017-01-10 10:06:09 +0900

    Add comment on each line of the code

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,22 +1,22 @@
 void setup() {
-  pinMode(13, OUTPUT); //set pin to 13 to output//
+  pinMode(13, OUTPUT); //set pin to 13 to output
 }
 
 void loop() {
-  digitalWrite(13, HIGH); //set LED to turn on//
-  delay(500); //set LED to turn on for half of a second//
-  digitalWrite(13, LOW); //set LED to turn off//
-  delay(500); //set LED to turn off for half of a second//
-  digitalWrite(13, HIGH);//set LED to turn on//
-  delay(1000); //set LED to turn on for one second//
-  digitalWrite(13, LOW);//set LED to turn off//
-  delay(1000); //set LED to turn off for one second//
-  digitalWrite(13, HIGH);//set LED to turn on//
-  delay(1500); //set LED to turn on for 1.5 second//
-  digitalWrite(13, LOW);//set LED to turn off//
-  delay(1500); //set LED to turn off for 1.5 second//
-  digitalWrite(13, HIGH);//set LED to turn on//
-  delay(2000); //set LED to turn on for two seconds//
-  digitalWrite(13, LOW);//set LED to turn off//
-  delay(2000); //set LED to turn off for two seconds//
+  digitalWrite(13, HIGH); //set LED to turn on
+  delay(500); //set LED to turn on for half of a second
+  digitalWrite(13, LOW); //set LED to turn off
+  delay(500); //set LED to turn off for half of a second
+  digitalWrite(13, HIGH);//set LED to turn on
+  delay(1000); //set LED to turn on for one second
+  digitalWrite(13, LOW);//set LED to turn off
+  delay(1000); //set LED to turn off for one second
+  digitalWrite(13, HIGH);//set LED to turn on
+  delay(1500); //set LED to turn on for 1.5 second
+  digitalWrite(13, LOW);//set LED to turn off
+  delay(1500); //set LED to turn off for 1.5 second
+  digitalWrite(13, HIGH);//set LED to turn on
+  delay(2000); //set LED to turn on for two seconds
+  digitalWrite(13, LOW);//set LED to turn off
+  delay(2000); //set LED to turn off for two seconds
 }
