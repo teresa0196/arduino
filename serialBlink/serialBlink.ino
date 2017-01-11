81e469b0995c7b097e30f4576da1714ab950bff2
Teresa Jeon <teresajeon@seoulchristian.org>
2017-01-11 10:10:07 +0900

    Edit code to show on and off in the serial monitor

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,5 +1,6 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
 }
 
@@ -7,6 +8,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, HIGH);
   delay(1000);
+  Serial.println("On");
   digitalWrite(13, LOW);
   delay(1000);
+  Serial.println("Off");
 }
