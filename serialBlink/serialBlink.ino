9d10a28db68324d669c6569e74be0e5dcf68819f
Teresa Jeon <teresajeon@seoulchristian.org>
2017-01-13 10:04:14 +0900

    Edit the code for accurate response of arduino

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,6 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("Hi! It's nice to see you:)");
   Serial.println("Send 'On' to turn on the LED.");
   Serial.println("Send 'Off' to turn off the LED.");
@@ -9,10 +10,17 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.readString() == "On") {
-    digitalWrite(13, HIGH);
+  if (Serial.available() > 0) {
+    String command;
+    command = Serial.readString();
+    if (command == "On") {
+      Serial.println("Turned on the light");
+      digitalWrite(13, HIGH);
+    }
+    else if (command == "Off") {
+      Serial.println("Turned off the light");
+      digitalWrite(13, LOW);
+    }
   }
-  else if (Serial.readString() == "Off") {
-    digitalWrite(13, LOW);
-  }
+
 }
