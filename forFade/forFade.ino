743f48ff95f7eb03ed3cd5f4d1de334d241f5da5
Teresa Jeon <teresajeon@seoulchristian.org>
2017-01-17 10:11:25 +0900

    Fade in and out the LED

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -1,10 +1,17 @@
 void setup() {
-Serial.begin(9600);
-pinMode(9,OUTPUT);
-
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-
-
+  for (int lightValue = 0; lightValue <= 255; lightValue++) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
+  }
+  for (int lightValue = 255; lightValue >= 0; lightValue--) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
+  }
 }
