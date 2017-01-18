af718805c550ed6368c891951384f2ff42259a5a
Teresa Jeon <teresajeon@seoulchristian.org>
2017-01-18 10:41:46 +0900

    Fade in and out the LED and print light value

--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -4,10 +4,15 @@ int delta = 1;
 void setup() {
   Serial.begin(9600);
   pinMode(9, OUTPUT);
-
-
 }
 
 void loop() {
-
+  lightValue = lightValue + delta;
+  if (lightValue <= 0 || lightValue >= 255) {
+    delta = -delta;
+  }
+  Serial.println(lightValue);
+  analogWrite(9, lightValue);
+  delay(10);
 }
+
