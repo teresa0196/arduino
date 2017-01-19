310113c4bf23ddd389f3f63cd41d56d06ba87c13
Teresa Jeon <teresajeon@seoulchristian.org>
2017-01-19 10:36:28 +0900

    Add comments to the programs

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -1,17 +1,17 @@
 void setup() {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600); // activate the serial connection
+  pinMode(9, OUTPUT); // set pin 9 to output 
 }
 
 void loop() {
-  for (int lightValue = 0; lightValue <= 255; lightValue++) {
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
+  for (int lightValue = 0; lightValue <= 255; lightValue++) { // set lightValue to 0 and increase it until it is equal to 255
+    analogWrite(9, lightValue); // set the brightness of the LED to the lightValue
+    Serial.println(lightValue); // print lightValue to the serial monitor
+    delay(10); // wait for ten milliseconds
   }
-  for (int lightValue = 255; lightValue >= 0; lightValue--) {
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
-  }
+  for (int lightValue = 255; lightValue >= 0; lightValue--) { // set lightValue to 255 and decrease it until it is equal to 0
+    analogWrite(9, lightValue); // set the brightness of the LED to the lightValue
+    Serial.println(lightValue); // print lightValue to the serial monitor
+    delay(10); // wait for ten milliseconds
+  } 
 }
--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -1,18 +1,18 @@
-int lightValue = 0;
-int delta = 1;
+int lightValue = 0; // set 'lightValue' to 0
+int delta = 1; // set 'delta' to 1
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600); // activate the serial connection
+  pinMode(9, OUTPUT); // set pin 9 to output
 }
 
 void loop() {
-  lightValue = lightValue + delta;
-  if (lightValue <= 0 || lightValue >= 255) {
-    delta = -delta;
+  lightValue = lightValue + delta; // set the value of 'lightValue' to the sum of 'lightValue' and 'delta'
+  if (lightValue <= 0 || lightValue >= 255) { // see if 'lightValue' is less or equal to 0 or greater or equal to 255
+    delta = -delta; // set 'delta' to negative
   }
-  Serial.println(lightValue);
-  analogWrite(9, lightValue);
-  delay(10);
-}
+  Serial.println(lightValue); // print lightValue to the serial monitor
+  analogWrite(9, lightValue); // set the brightness of the LED to the value of 'lightValue'
+  delay(10); // wait for ten milliseconds
+} 
 
